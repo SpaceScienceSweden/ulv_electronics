74645e3bb8683c2a3b5fcbb8b2bc203fee5ec139
Tomas Härdin <tjoppen@acc.umu.se>
2016-04-11 22:32:21 +0200

    Move ulv_libraries to ulv_electronics/
    This also makes the URLs in .gitmodules relative so checkout works fine regardless of the user using https or ssh clone

--- test.sh
+++ test.sh
@@ -1,24 +1,2 @@
 set -e  # Stop on errors
-LIBRARIES="c4f8ab79a81315e859af9297f28c5d6179aee187"
-
-for d in ulv_*
-do
-    (
-    echo "Checking $d"
-    cd $d
-
-    # First check if libraries submodule is dirty
-    git diff --exit-code ulv_libraries
-
-    # OK, make sure we're on the right commit
-    cd ulv_libraries
-    COMMIT="$(git rev-parse HEAD)"
-    if [ "$LIBRARIES" != "$COMMIT" ]
-    then
-        echo "$d uses wrong libraries: found $COMMIT expected $LIBRARIES"
-        exit 1
-    fi
-    )
-done
-
 (cd ulv_beacon && ./test.sh)
