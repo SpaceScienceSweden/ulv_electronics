7751e32d8e8e3c83f4ec67be6f022f3054c2b9c8
Tomas Härdin <tjoppen@acc.umu.se>
2016-09-05 13:02:45 +0200

    PWM out, TACH in
    We're quickly running into the limits of the Arduino libraries with this one.

--- cpu/software_demodulator/software_demodulator.ino
+++ cpu/software_demodulator/software_demodulator.ino
@@ -6,12 +6,16 @@
 #define PAIRS            3  //number of sensor pairs
 #define SAMPLES_PER_REV (PAIRS*8)
 #define MICROS_PER_INTERRUPT (MICROS_PER_REV / SAMPLES_PER_REV)
+#define PWMPIN 12
+#define TACHPIN 2
 
 unsigned char cursample = 0;
 char sintab[256];
 long I[PAIRS] = {0}, Q[PAIRS] = {0};
 boolean got_sample = false;
 unsigned long dc;
+unsigned long nextpwm = 0;
+char pwmstate = 0;
 
 static void interrupt() {
   //disable interrupts
@@ -25,8 +29,8 @@ static void interrupt() {
   //NOTE: analogRead() is measured as taking 112 µs (9600 Hz)
   //      keep this in mind
   int a = analogRead(0);
-  int b = analogRead(1);
-  int c = analogRead(2);
+  int b = 0;//analogRead(1);
+  int c = 0;//analogRead(2);
 
   //re-enable interrupts
   cli();
@@ -45,7 +49,7 @@ static void interrupt() {
   cursample++;
 
   if (cursample >= SAMPLES_PER_REV * 10) {
-    got_sample = true;
+    //got_sample = true;
     cursample = 0;
   }
   
@@ -58,6 +62,15 @@ const unsigned char PS_32 = (1 << ADPS2) | (1 << ADPS0);
 const unsigned char PS_64 = (1 << ADPS2) | (1 << ADPS1);
 const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 
+volatile unsigned long last_interrupt;
+volatile unsigned long last_interrupt_length;
+static void tach_interrupt() {
+  unsigned long us = micros();
+  last_interrupt_length = us - last_interrupt;
+  last_interrupt = us;
+  //TODO: setup timer interrupt using last_interrupt_length / 6
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello");
@@ -67,6 +80,10 @@ void setup() {
   }
   Timer1.initialize(MICROS_PER_INTERRUPT);
   Timer1.attachInterrupt(interrupt);
+  pinMode(PWMPIN, OUTPUT);
+  analogReference(INTERNAL);
+  
+  attachInterrupt(INT0, tach_interrupt, RISING);
 
   //trimma ADC:n om nödvändigt
   //atmel säger att upp till 1 MHz ADC-klocka kan vara OK (ung. 80 kHz)
@@ -74,18 +91,35 @@ void setup() {
   //ADCSRA |= PS_32;
 }
 
+//int a;
 void loop() {
+  unsigned long us = micros();
+  //a++;
+  if (us >= nextpwm) {
+    digitalWrite(PWMPIN, (us / 1000) & 1);
+    if (pwmstate) {
+      digitalWrite(PWMPIN, LOW);
+      pwmstate = 0;
+      nextpwm = us + 20000;
+    } else {
+      digitalWrite(PWMPIN, HIGH);
+      pwmstate = 1;
+      nextpwm = us + (us > 1000000 ? 1150 : 1000);
+    }
+  }
+  
   //Serial.println("Loop");
-  if (got_sample) {
+  /*if (got_sample) {
     char tmp[256];
-    snprintf(tmp, 256, "sample: %9li %9li, ISR time: %li vs %i", I[0], Q[0], dc, MICROS_PER_INTERRUPT);
-    Serial.println(tmp);
+    //snprintf(tmp, 256, "sample: %9li %9li, ISR time: %li vs %i", I[0], Q[0], dc, MICROS_PER_INTERRUPT);
+    //snprintf(tmp, 256, "us = %li\n", us);
+    //Serial.println(tmp);
     int x;
     for (x = 0; x < 3; x++) {
       I[x] = 0;
       Q[x] = 0;
     }
     got_sample = 0;
-  }
+  }*/
 }
 
