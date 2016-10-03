de5d172e7edce17e408de10e3d12b509cc906103
Tomas Härdin <tjoppen@acc.umu.se>
2016-10-03 13:38:07 +0200

    Good enough

--- adc1/filter.m
+++ adc1/filter.m
@@ -1,12 +1,12 @@
 % NP0's only come up to 0.47 µF, and then they're expensive and big.
 % Kemet's are 2220 (5.6x5.0 mm)
 % TODO: Take the capacitor on the first stage amplifier into account
-R1=33e3;
+R1=18e3;
 C1=220e-9;
 R2=R1;      % unit gain
-C2=470e-12;
+C2=1e-9;
 R3=1e3;     % also protects ADC input
-C3=10e-9;
+C3=18e-9;
 
 % filter cutoff frequencies
 f1=1./(2*pi*R1*C1)
@@ -18,8 +18,6 @@ fadc=8e6;
 fmod=fadc/4
 N=256
 srate=fmod/N
-%srate * 1.5 rounded to 100 Hz
-alias_max_freq=100*round(srate*1.5/100);
 
 %f=10.^(0:0.001:log10(fmod));
 f=[1:99,100:10:990,1000:100:fmod];
@@ -39,7 +37,7 @@ g2 = abs(g2);
 finalp = 180/pi*angle(final);
 final = abs(final);
 
-alias_max_dB = 10*log10(final(find(f==alias_max_freq)))
+alias_max_dB = 10*log10(max(final(find(f==100*ceil(srate/100)):end)))
 
 subplot(2,2,1:2);
 semilogx(f,10*log10(g1),'-',f,10*log10(g2),'-',f,10*log10(sinc3),'-',f,10*log10(final),'-');
@@ -51,9 +49,9 @@ axis([min(f),max(f),-100,0])
 subplot(2,2,3);
 ax = plotyy(f,10*log10(final),f,finalp);
 fmin = 100;
-fmax = 1000;
-axis(ax(1), [fmin,fmax,-3,0]);
-axis(ax(2), [fmin,fmax,-30,30]);
+fmax = srate/2;
+axis(ax(1), [fmin,fmax,-10,0]);
+axis(ax(2), [fmin,fmax,-50,50]);
 title(sprintf('Closeup, %.0f-%.0f Hz', fmin, fmax))
 xlabel('frequency (Hz)')
 ylabel(ax(1), "Amplitude (dB)");
