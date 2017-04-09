8a437931c1d57d99b2b7d9be4041be632e453683
Tomas Härdin <tjoppen@acc.umu.se>
2017-04-09 23:31:19 +0200

    fieldmill_top_plate3: Remove spokes, bigger via ring, slightly larger r2

--- fieldmill_top_plate3/generate_circle_segments.py
+++ fieldmill_top_plate3/generate_circle_segments.py
@@ -4,15 +4,15 @@ import math
 cx = 100
 cy = 100
 
-spokes = 4
 clearance = 0.2
 segmentclearance = 0.15 #for DRC
 #width of inner gnd ring (between motor hole and pie pieces)
-ring = 0.8
+ring = 1.6
 
 #pie pieces
 r1 = 11.25 + ring + 2*clearance
-r2 = 39
+r2 = 40
+# 746 mm^2
 
 rr = r1 - clearance - ring/2
 
@@ -44,11 +44,10 @@ def draw_slice(r1, r2, a1, a2, clearance1, clearance2, segments):
 for i in range(6):
     a1 = 2*math.pi*(i+1)/6
     a2 = 2*math.pi*(i+2)/6
-    spokea = -clearance if i % 2 == 1 else spokes
-    spokeb = -clearance if i % 2 == 0 else spokes
 
     print('''
   (zone (net %i) (net_name "/SENSOR%i") (layer F.Cu) (tstamp 0) (hatch edge 0.508)
+    (priority 1)
     (connect_pads yes (clearance %f))
     (min_thickness 0.254)
     (fill yes (arc_segments 16) (thermal_gap 0.508) (thermal_bridge_width 0.508))
@@ -56,54 +55,7 @@ for i in range(6):
       (pts
 ''' % (i+2,i+1, segmentclearance))
 
-    draw_slice(r1, r2, a1, a2, spokeb+2*clearance, spokea+2*clearance, segments)
-    print('''
-      )
-    )
-  )
-''')
-
-    if spokea > 0:
-        #spoke
-        print('''
-  (zone (net 1) (net_name "GND") (layer F.Cu) (tstamp 0) (hatch edge 0.508)
-    (connect_pads yes (clearance %f))
-    (min_thickness 0.254)
-    (fill yes (arc_segments 16) (thermal_gap 0.508) (thermal_bridge_width 0.508))
-    (polygon
-      (pts
-''' % segmentclearance)
-
-        draw_slice(r1-clearance-ring, r2+clearance+1, a1, a1, spokea, spokea, 4)
-        print('''
-      )
-    )
-  )
-''')
-    
-
-for i in range(4):
-    a1 = math.pi/2*i
-    a2 = math.pi/2*(i+1)
-
-    print('''
-  (zone (net 1) (net_name "GND") (layer F.Cu) (tstamp 0) (hatch edge 0.508)
-    (connect_pads yes (clearance %f))
-    (min_thickness 0.254)
-    (fill yes (arc_segments 16) (thermal_gap 0.508) (thermal_bridge_width 0.508))
-    (polygon
-      (pts
-        (xy %f %f)
-        (xy %f %f)
-        (xy %f %f)
-''' % (
-    segmentclearance,
-    cx + edge*math.cos(a2), cy + edge*math.sin(a2),
-    cx + math.sqrt(2)*edge*math.cos(0.5*(a1+a2)), cy + math.sqrt(2)*edge*math.sin(0.5*(a1+a2)),
-    cx + edge*math.cos(a1), cy + edge*math.sin(a1),
-))
-
-    draw_arc(r2+clearance, a1, a2, segments)
+    draw_slice(r1, r2, a1, a2, clearance, clearance, segments)
     print('''
       )
     )
@@ -124,11 +76,12 @@ for i in range(nring):
     for j in range(m):
         a3 = a1 + (a2 - a1)*j/m
         a4 = a1 + (a2 - a1)*(j+1)/m
-        print('  (segment (start %f %f) (end %f %f) (width %f) (layer F.Cu) (net 1))' % (
-            cx + rr*math.cos(a3), cy + rr*math.sin(a3),
-            cx + rr*math.cos(a4), cy + rr*math.sin(a4),
-            ring
-        ))
+        for layer in ['F.Cu','B.Cu']:
+            print('  (segment (start %f %f) (end %f %f) (width %f) (layer %s) (net 1))' % (
+                cx + rr*math.cos(a3), cy + rr*math.sin(a3),
+                cx + rr*math.cos(a4), cy + rr*math.sin(a4),
+                ring, layer
+            ))
 
 print('''
   (module SCUBE:M3_PTH (layer F.Cu) (tedit 57ADC4E8) (tstamp 584875C3)
