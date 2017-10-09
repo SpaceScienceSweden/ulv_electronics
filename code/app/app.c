66b0e12291016cab27916640665f4bea9048cd44
Tomas Härdin <tjoppen@acc.umu.se>
2017-10-09 13:34:28 +0200

    Add EEPROM
    The purpose here is to keep the motors running in the bootloader to reduce bearing wear.
    "make flight" initializes the EEPROM so that the motors start OFF.
    The application is responsible for writing the current motor speed to EEPROM to keep them from stopping.

--- code/app/app.c
+++ code/app/app.c
@@ -47,6 +47,18 @@
 #define UART_CTRL2_DATA	((1<<UCSZ11) | (1<<UCSZ10))
 #define UART_DATA	UDR1
 
+typedef struct {
+  uint8_t gap;            //gap to make it easier to see where the data in eeprom.hex starts
+  uint8_t motor_pwm_set;  //if not FF then motor PWMs are set
+  uint8_t pwm0;           //OCR1A
+  uint8_t pwm1;           //OCR1B
+  uint8_t pwm2;           //OCR1C
+
+  //here's how you would set eeprom.hex for running all motors at 25% speed (0x40 == 64):
+  //:20000000FF00404040FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF3C
+  //:00000001FF
+} eeprom_t;
+
 static void enable_tx(void) {
   UART_CTRL = UART_CTRL_DATA_TX;
   RS485_DE_PORT |= RS485_DE_BIT;
