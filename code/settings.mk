c8d3fc8a2ede60ffc5d8ecc48738720a28a74e98
Tomas Härdin <tjoppen@acc.umu.se>
2019-03-04 14:21:15 +0100

    Add settings.mk, make app/ Makefile usable on its own

---
+++ code/settings.mk
@@ -0,0 +1,11 @@
+MCU=atmega128
+AVRDUDE_PROGRAMMER=stk600
+#TODO: BOOTSIZE=512 is enough for flight
+BOOTSIZE=1024
+# Baudrate in program and bootloader
+BAUD=115200
+# Actual baudrate used during programming
+# Will be different from BAUD while changing rates
+PROGBAUD=115200
+CFLAGS_ROOT=-DF_CPU=7372800LL -DBAUD=$(BAUD) -DBOOTSIZE=$(BOOTSIZE)
+
