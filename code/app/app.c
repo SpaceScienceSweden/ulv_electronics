94314b85e5e60422146cdf0dca05b2775b3b2448
Tomas Härdin <tjoppen@acc.umu.se>
2019-04-17 11:17:09 +0200

    Remove the emojis
    They crash codelite

--- code/app/app.c
+++ code/app/app.c
@@ -1675,7 +1675,7 @@ static void handle_input(void) {
       } else if (c == '?') {
         //print help
         start_section("INFO");
-        printf_P(PSTR("Read manual.pdf í¼ í¹œr\n"));
+        printf_P(PSTR("Read manual.pdf\r\n"));
         return;
       }
 }
@@ -1729,7 +1729,7 @@ int main(void)
   set_vgnd(2, vgnds[2]);
 
   start_section("INFO");
-  printf_P(PSTR("Hello, Earth! í¼‡í½œr\n"));
+  printf_P(PSTR("Hello, Earth!r\n"));
 
 #if FEATURE_MMC == 1
   //must be after sei() because timers
